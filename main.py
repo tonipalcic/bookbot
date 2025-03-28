5056f9dc64b3a27dcc194ceac9e614e39d6eb4a0
tonipalcic <tonipalcic@hotmail.com>
2025-03-28 20:10:21 +0100

    CH2 L6

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, character_count
 
 
 def get_book_text(filepath: str) -> str:
@@ -12,6 +12,7 @@ def main():
     text = (get_book_text(path))
     num_words = get_num_words(text)
     print(f'{num_words} words found in the document')
+    print(character_count(text))
 
 
 
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_num_words(text: str)->int:
     return len(text.split())
-   
+
+def character_count(text: str)-> dict[str, int]:
+   d = dict()
+   text = text.lower()
+   for c in text:
+       if c not in d:
+           d[c] = 1
+       else:
+          d[c] +=1
+    
+   return d
