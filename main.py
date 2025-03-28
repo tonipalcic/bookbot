5d51c64baf15649deccb29651d64860594b5dc7a
tonipalcic <tonipalcic@hotmail.com>
2025-03-28 19:46:02 +0100

    CH2 L3

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+def get_book_text(filepath: str) -> str:
+    with open(filepath) as f:
+        text =  f.read()
+    f.close()
+    return text
+
+def main():
+    path = "books/frankenstein.txt"
+    print(get_book_text(path))
+
+
+
+
+
+if __name__ == "__main__":
+    main()
