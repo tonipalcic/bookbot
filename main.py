4406fe222bbb066f2f938b5ce3183ede50d102b8
tonipalcic <tonipalcic@hotmail.com>
2025-03-28 20:55:09 +0100

    CH3 L1

--- main.py
+++ main.py
@@ -10,9 +10,23 @@ def get_book_text(filepath: str) -> str:
 def main():
     path = "books/frankenstein.txt"
     text = (get_book_text(path))
-    num_words = get_num_words(text)
-    print(f'{num_words} words found in the document')
-    print(character_count(text))
+    word_count = get_num_words(text)
+    char_dict = character_count(text)
+    
+    print_string = f'''============ BOOKBOT ============
+Analyzing book found at {path}...
+----------- Word Count ----------
+Found {word_count} total words
+--------- Character Count -------\n'''
+    
+    sorted_chars = dict(sorted(((k, v) for k, v in char_dict.items() if k.isalnum()), key=lambda x: x[1], reverse=True))
+
+    for key, value in sorted_chars.items():
+        print_string += f"{key}: {value}\n"
+
+    print_string += "============= END ==============="
+
+    print(print_string)
 
 
 
