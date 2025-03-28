a342674cea670ee5bf8d9a311b54234c2b0ddea2
tonipalcic <tonipalcic@hotmail.com>
2025-03-28 19:53:23 +0100

    CH2 L4

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ def get_book_text(filepath: str) -> str:
 
 def main():
     path = "books/frankenstein.txt"
-    print(get_book_text(path))
+    text = (get_book_text(path))
+    num_words = len(text.split())
+    print(f'{num_words} words found in the document')
 
 
 
